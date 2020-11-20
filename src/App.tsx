5d591be246f71f5396abac283e4203003af78e26
y4shiro <y4shironao@gmail.com>
2020-11-20 22:36:26 +0900

    add CounterWithReducer.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Counter from './Counter';
+import CounterWithReducer from './CounterWithReducer';
 
 interface AppProps {
   message?: string;
@@ -8,7 +8,7 @@ interface AppProps {
 const App: React.FunctionComponent<AppProps> = ({ message }) => {
   return (
     <div>
-      <Counter />
+      <CounterWithReducer />
     </div>
   );
 };
@@ -16,6 +16,6 @@ const App: React.FunctionComponent<AppProps> = ({ message }) => {
 App.defaultProps = {
   message: 'Hello, defaultProps!',
   // description: 'This is App component.'
-}
+};
 
 export default App;
